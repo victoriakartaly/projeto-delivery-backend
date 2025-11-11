ba2844f74b279780801dec06ab8d8e2d8bccffe6
Gustavo-cabral-01 <gustavocabral72outlook.com@gmail.com>
2025-11-11 16:43:48 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
 
 import mongoose from "mongoose";
-import "dotenv/config"; // Importa e configura o dotenv em uma linha só.
+import "dotenv/config"; 
 
-// Usando 'export const' para declarar e exportar a função de uma vez
 export const connectDB = async () => {
     try {
         // Acessa a variável de ambiente
@@ -10,7 +9,7 @@ export const connectDB = async () => {
         console.log("✅ Conectado ao MongoDB com sucesso!");
     } catch (err) {
         console.error("❌ Erro ao conectar no MongoDB:", err.message);
-        // Em caso de falha crítica, encerra o processo
+        
         process.exit(1);
     } 
 };
