fbf1016fc1240c7705aeacfad4633d2bc88f6cda
victoriakartaly <159584442+victoriakartaly@users.noreply.github.com>
2025-11-11 16:29:02 -0300

    Update auth.middleware.js
    Formatação

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,27 +1,27 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 
-// Função utilitária para gerar o JWT (usada no Controller)
+
 export const generateToken = (id) => {
     
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '7d', // Token expira em 7 dias
+        expiresIn: '7d', 
     });
 };
 
-// 1. Protege a rota: Verifica se o usuário está logado
+
 export const protect = async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            // Obtém o token do cabeçalho
+            
             token = req.headers.authorization.split(' ')[1];
 
-            // Verifica o token
+            
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            // Anexa o usuário à requisição (exceto a senha)
+            
             req.user = await User.findById(decoded.id).select('-password');
 
             if (!req.user) {
@@ -40,7 +40,7 @@ export const protect = async (req, res, next) => {
     }
 };
 
-// 2. Autoriza por Perfil: Restringe acesso a certos perfis
+
 
 export const authorize = (...roles) => {
     return (req, res, next) => {
