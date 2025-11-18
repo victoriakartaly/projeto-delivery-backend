e3cee147490eda4341f4712435db43582eba4281
victoriakartaly <159584442+victoriakartaly@users.noreply.github.com>
2025-11-18 08:18:36 -0300

    Update database.js
    Organização de código.

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,23 @@
-
-import mongoose from "mongoose";
+import mongoose from "mongoose"; 
 import "dotenv/config"; 
 
+
 export const connectDB = async () => {
+
     try {
-        
-        await mongoose.connect(process.env.MONGO_URI);
+       
+      
+
+        await mongoose.connect(process.env.MONGO_URI); 
+
+       
+
         console.log("✅ Conectado ao MongoDB com sucesso!");
+
+
     } catch (err) {
         console.error("❌ Erro ao conectar no MongoDB:", err.message);
+       
         
         process.exit(1);
     } 
